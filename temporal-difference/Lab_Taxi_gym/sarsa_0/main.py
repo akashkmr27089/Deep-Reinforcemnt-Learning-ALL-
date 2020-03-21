c098af9c26b6f1a63834f1d2d4ccea20eceaad4e
akashkmr27089 <akashkmr27089@gmail.com>
2020-03-21 20:46:31 +0530

    added sarsa

---
+++ temporal-difference/Lab_Taxi_gym/sarsa_0/main.py
@@ -0,0 +1,8 @@
+from agent import Agent
+from monitor import interact
+import gym
+import numpy as np
+
+env = gym.make('Taxi-v3')
+agent = Agent(alpha = 0.4762 , eps_start = 0.01268, epsilon = 0.04973 , gamma = 0.9662)
+avg_rewards, best_avg_reward = interact(env, agent)
