0fa9469e3e8c130ecaeef13de007e52da96263f2
akashkmr27089 <akashkmr27089@gmail.com>
2020-05-31 23:51:50 +0530

    editing

---
+++ p1_navigation/Navigation.py
@@ -0,0 +1,144 @@
+from unityagents import UnityEnvironment
+import numpy as np
+
+env = UnityEnvironment(file_name="C:/Users/akash/Desktop/Banana_Windows_x86_64/Banana.exe")
+
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space 
+state = env_info.vector_observations[0]
+print('States look like:', state)
+state_size = len(state)
+print('States have length:', state_size)
+
+env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+state = env_info.vector_observations[0]            # get the current state
+score = 0                                          # initialize the score
+while True:
+    action = np.random.randint(action_size)        # select an action
+    env_info = env.step(action)[brain_name]        # send the action to the environment
+    next_state = env_info.vector_observations[0]   # get the next state
+    reward = env_info.rewards[0]                   # get the reward
+    done = env_info.local_done[0]                  # see if episode has finished
+    score += reward                                # update the score
+    state = next_state                             # roll over the state to next time step
+    if done:                                       # exit loop if episode finished
+        break
+
+# initialize the score
+for i in range(3):
+    env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+    state = env_info.vector_observations[0]            # get the current state
+    score = 0   
+    while True:
+        action = np.random.randint(action_size)        # select an action
+        print(action)
+        env_info = env.step(action)[brain_name]        # send the action to the environment
+        next_state = env_info.vector_observations[0]   # get the next state
+        reward = env_info.rewards[0]                   # get the reward
+        done = env_info.local_done[0]                  # see if episode has finished
+        score += reward                                # update the score
+        state = next_state                             # roll over the state to next time step
+        if done:                                       # exit loop if episode finished
+            break
+env.close()
+
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+from dqn_agent import Agent
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+agent = Agent(state_size=37, action_size=4, seed=0)
+
+
+def dqn(n_episodes=2000, max_t=100, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+        state = env_info.vector_observations[0]            # get the current state
+        score = 0                                          # initialize the score
+        for _ in range(max_t):
+            action = agent.act(state, eps)
+#             print(type(action))
+            env_info = env.step(action)[brain_name]
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            agent.step(state, action, reward, next_state, done)
+            state = next_state
+            score += reward
+            if done:
+                break
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 1 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=200.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpoint.pth')
+            break
+    return scores
+
+scores = dqn()
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(np.arange(len(scores)), scores)
+plt.ylabel('Score')
+plt.xlabel('Episode #')
+plt.show()
+
+torch.save(agent.qnetwork_local.state_dict(), './bananamodel1')
+
+model = TheModelClass(*args, **kwargs)
+model.load_state_dict(torch.load(PATH))
+model.eval()
+
+env_info = env.step(action)[brain_name]
+
+
+for i in range(3):
+    env_info = env.reset(train_mode=False)[brain_name] # reset the environment
+    state = env_info.vector_observations[0]            # get the current state
+    score = 0   
+    while True:
+        action = agent.act(state)        # select an action
+        env_info = env.step(action)[brain_name]        # send the action to the environment
+        next_state = env_info.vector_observations[0]   # get the next state
+        reward = env_info.rewards[0]                   # get the reward
+        done = env_info.local_done[0]                  # see if episode has finished
+        score += reward                                # update the score
+        print('\rScore_value {}'.format(score), end = ' ')
+        state = next_state                             # roll over the state to next time step
+        if done:                                       # exit loop if episode finished
+            break
+    print()
+
+env.close()
